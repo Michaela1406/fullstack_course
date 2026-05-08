4e76183824eee7abf24a2f4a443d6a63fbe29480
Michaela Weiss <michaelaweiss@Michaelas-MacBook-Pro-3.local>
2026-05-08 15:33:47 +0300

    friday 8.5.2026 second part

---
+++ part4/bloglist-backend/index.js
@@ -0,0 +1,54 @@
+// Exercise 4.1 Blog list step 1
+
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+.then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
