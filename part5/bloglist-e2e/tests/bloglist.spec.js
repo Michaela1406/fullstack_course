920a16b0a203823a1c6da07047b6edfddd2a62b3
Michaela Weiss <michaelaweiss@Michaelas-MacBook-Pro-2.local>
2026-08-19 15:44:49 +0300

    wednesday 19.8.2026 first part

--- part5/bloglist-e2e/tests/bloglist.spec.js
+++ part5/bloglist-e2e/tests/bloglist.spec.js
@@ -25,7 +25,7 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await page.goto('http://localhost:5173')
 
-    const locator = page.getByText('Log in to application')
+    const locator = page.getByRole("heading", {name: 'Blogs'} )
     await expect(locator).toBeVisible()
   })
 
@@ -33,12 +33,15 @@ describe('Blog app', () => {
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
       await loginWith(page, 'ElvisTax', 'hemligt')
+      await expect(page.getByRole('button', {name: 'logout'})).toBeVisible()
+      await expect(page.getByRole("heading", {name: 'Blogs'} )).toBeVisible()
       await expect(page.getByText('ElvisTax logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
       await loginWith(page, 'ElvisTax', 'wrong')
       await expect(page.getByText('Wrong username or password')).toBeVisible()
+      await expect(page.getByRole("heading", {name: 'Blogs'} )).toBeVisible()
     })
   })
 
@@ -55,36 +58,34 @@ describe('Blog app', () => {
     // Exercise 5.20 Blog List End To End Testing, step 4
     test('and the blog can be liked', async ({ page }) => {
         await createBlog(page, 'Elvis testing playwright', 'Elvis', 'www.elvistax.com')
-        await page.getByRole('button', { name: 'view' }).click()
+        await page.getByRole('link',{name: 'Elvis testing playwright by Elvis'}).click()
         await page.getByRole('button', { name: 'like' }).click()
-        await expect(page.getByText('likes: 1')).toBeVisible()
+        await expect(page.getByText('likes 1')).toBeVisible()
     })
 
     // Exercise 5.21 Blog List End To End Testing, step 5
     test('and the blog can be deleted', async ({ page }) => {
         await createBlog(page, 'Elvis testing playwright', 'Elvis', 'www.elvistax.com')
-        await page.getByRole('button', { name: 'view' }).click()
+        await page.getByRole('link',{name: 'Elvis testing playwright by Elvis'}).click()
         page.on('dialog', async dialog => {
             console.log('Dialog: ', dialog.message());
         await dialog.accept();
        });
         await page.getByRole('button', { name: 'remove' }).click()
-        await expect(page.getByText('Elvis testing playwright - author: Elvis')).not.toBeVisible()
+        await expect(page.getByRole('link',{name: 'Elvis testing playwright by Elvis'})).not.toBeVisible()
     })
 
     // Exercise 5.22 Blog List End To End Testing, step 6
     test('only the user who created a blog can see the delete button', async ({ page, request }) => {
         await createBlog(page, 'Elvis testing playwright', 'Elvis', 'www.elvistax.com')
         await page.getByText('ElvisTax logged in').waitFor()
-        await expect(page.getByText('Elvis testing playwright - author: Elvis')).toBeVisible()
-        await page.getByRole('button', { name: 'view' }).click()
+        await page.getByRole('link',{name: 'Elvis testing playwright by Elvis'}).click()
         await expect(page.getByRole('button', { name: 'remove' })).toBeVisible()
         await page.getByRole('button', { name: 'logout' }).click()
 
         await loginWith(page, 'OonaTax', 'hemligt')
         await page.getByText('OonaTax logged in').waitFor()
-        await expect(page.getByText('Elvis testing playwright - author: Elvis')).toBeVisible()
-        await page.getByRole('button', { name: 'view' }).click()
+        await page.getByRole('link',{name: 'Elvis testing playwright by Elvis'}).click()
         await expect(page.getByRole('button', { name: 'remove' })).not.toBeVisible()
     })
 
@@ -94,50 +95,49 @@ describe('Blog app', () => {
         await createBlog(page, 'Oona testing playwright', 'Oona', 'www.oonatax.com')
         await createBlog(page, 'Lando testing playwright', 'Lando', 'www.landovovve.com')
 
-        await page.getByText('Elvis testing playwright - author: Elvis').waitFor()
-        await page.getByText('Oona testing playwright - author: Oona').waitFor()
-        await page.getByText('Lando testing playwright - author: Lando').waitFor()
+        await page.getByRole('link',{name: 'Elvis testing playwright by Elvis'}).waitFor()
+        await page.getByRole('link',{name: 'Oona testing playwright by Oona'}).waitFor()
+        await page.getByRole('link',{name: 'Lando testing playwright by Lando'}).waitFor()
 
-        const elvisBlog = await page.getByText('Elvis testing playwright - author: Elvis').locator('..')
-        const oonaBlog = await page.getByText('Oona testing playwright - author: Oona').locator('..')
-        const landoBlog = await page.getByText('Lando testing playwright - author: Lando').locator('..')
+        await page.getByRole('link',{name: 'Elvis testing playwright by Elvis'}).click() 
+        await expect(page.getByRole('heading', {name: 'Elvis testing playwright by Elvis'})).toBeVisible()  
+        await page.getByRole('button', { name: 'like' }).click()
+        await page.getByText('Likes 1').waitFor()
+        await expect(page.getByText('Likes 1')).toBeVisible()
+        await page.getByRole('link', { name: 'blogs' }).click()
+        await expect(page.getByRole("heading", {name: 'Blogs'} )).toBeVisible()
 
-        await elvisBlog.getByRole('button', { name: 'view' }).click() 
-        const elvisOpenBlog = await page.getByText('Elvis testing')   
-        await elvisOpenBlog.getByRole('button', { name: 'like' }).click()
-        await elvisOpenBlog.getByText('Likes: 1').waitFor()
-        expect(elvisOpenBlog.getByText('Likes: 1')).toBeVisible()
-        await elvisOpenBlog.getByRole('button', { name: 'hide' }).click()
+        await page.getByRole('link',{name: 'Lando testing playwright by Lando'}).click()
+        await expect(page.getByRole('heading', {name: 'Lando testing playwright by Lando'})).toBeVisible()
+        await page.getByRole('button', { name: 'like' }).click()
+        await page.getByText('Likes 1').waitFor()
+        await page.getByRole('button', { name: 'like' }).click()
+        await page.getByText('Likes 2').waitFor()
+        expect(page.getByText('Likes 2')).toBeVisible()
+        await page.getByRole('link', { name: 'blogs' }).click()
+        await expect(page.getByRole("heading", {name: 'Blogs'} )).toBeVisible()
 
-        await landoBlog.getByRole('button', { name: 'view' }).click()
-        const landoOpenBlog = await page.getByText('Lando testing')
-        await landoOpenBlog.getByRole('button', { name: 'like' }).click()
-        await landoOpenBlog.getByText('Likes: 1').waitFor()
-        await landoOpenBlog.getByRole('button', { name: 'like' }).click()
-        await landoOpenBlog.getByText('Likes: 2').waitFor()
-        expect(landoOpenBlog.getByText('Likes: 2')).toBeVisible()
-        await landoOpenBlog.getByRole('button', { name: 'hide' }).click()
+        await page.getByRole('link',{name: 'Oona testing playwright by Oona'}).click()
+        await expect(page.getByRole('heading', {name: 'Oona testing playwright by Oona'})).toBeVisible()
+        await page.getByRole('button', { name: 'like' }).click()
+        await page.getByText('Likes 1').waitFor()
+        await page.getByRole('button', { name: 'like' }).click()
+        await page.getByText('Likes 2').waitFor()
+        await page.getByRole('button', { name: 'like' }).click()
+        await page.getByText('Likes 3').waitFor()
+        expect(page.getByText('Likes 3')).toBeVisible()
+        await page.getByRole('link', { name: 'blogs' }).click()
+        await expect(page.getByRole("heading", {name: 'Blogs'} )).toBeVisible()
 
-        await oonaBlog.getByRole('button', { name: 'view' }).click()
-        const oonaOpenBlog = await page.getByText('Oona testing')
-        await oonaOpenBlog.getByRole('button', { name: 'like' }).click()
-        await oonaOpenBlog.getByText('Likes: 1').waitFor()
-        await oonaOpenBlog.getByRole('button', { name: 'like' }).click()
-        await oonaOpenBlog.getByText('Likes: 2').waitFor()
-        await oonaOpenBlog.getByRole('button', { name: 'like' }).click()
-        await oonaOpenBlog.getByText('Likes: 3').waitFor()
-        expect(oonaOpenBlog.getByText('Likes: 3')).toBeVisible()
-        await oonaOpenBlog.getByRole('button', { name: 'hide' }).click()
-        await page.getByText('Oona testing playwright - author: Oona').waitFor()
+        const blogs = await page.getByRole('listitem').all()
+        console.log('blogs:', blogs)
+        const firstBlog = await blogs[0]
+        const secondBlog = await blogs[1]
+        const thirdBlog = await blogs[2]
 
-        const blogs = await page.locator('.blog')
-        const firstBlog = await blogs.nth(0)
-        const secondBlog = await blogs.nth(1)
-        const thirdBlog = await blogs.nth(2)
-
-        await expect(firstBlog).toContainText('Oona testing playwright - author: Oona')
-        await expect(secondBlog).toContainText('Lando testing playwright - author: Lando')
-        await expect(thirdBlog).toContainText('Elvis testing playwright - author: Elvis')
+        await expect(firstBlog).toContainText('Oona testing playwright by Oona')
+        await expect(secondBlog).toContainText('Lando testing playwright by Lando')
+        await expect(thirdBlog).toContainText('Elvis testing playwright by Elvis')
 
     })    
   })
--- part5/bloglist-e2e/tests/helper.js
+++ part5/bloglist-e2e/tests/helper.js
@@ -1,4 +1,8 @@
+const {expect} = require('@playwright/test')
+
 const loginWith = async (page, username, password)  => {
+  await page.getByRole('link', {name: 'login'}).click();
+  await expect(page.getByRole('heading', {name: 'Log in to application'})).toBeVisible()
   await page.getByRole('button', { name: 'login' }).click()
   await page.getByLabel('username').fill(username)
   await page.getByLabel('password').fill(password)
@@ -6,12 +10,14 @@ const loginWith = async (page, username, password)  => {
 }
 
 const createBlog = async (page, title, author, url) => {
-    await page.getByRole('button', { name: 'create new blog' }).click()
+    await page.getByRole('link', { name: 'create blog' }).click()
     await page.getByLabel('title').fill(title)
     await page.getByLabel('author').fill(author)
     await page.getByLabel('url').fill(url)
     await page.getByRole('button', { name: 'create' }).click()
-    await page.getByText(title + ' - author: ' + author).waitFor()
+    await page.getByRole('link', { name: 'blogs' }).click()
+    await expect(page.getByRole("heading", {name: 'Blogs'} )).toBeVisible()
+    await expect(page.getByRole('link',{name: title + ' by ' + author})).toBeVisible()
 }
 
 export { loginWith, createBlog }
